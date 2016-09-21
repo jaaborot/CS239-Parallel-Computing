0158ffc6cd0fc85e23cd90f025f7274dcd7554d0
jaaborot <jaaborot@up.edu.ph>
2016-09-21 18:11:22 +0800

    Created skeleton for main function.

---
+++ Matrix_Multiplication/src/Matrix_Multiplication.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Matrix_Multiplication.c
+ Author      : Jeffrey A. Aborot
+ Version     :
+ Copyright   : This work is open-source but requires proper citation if to be used.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
