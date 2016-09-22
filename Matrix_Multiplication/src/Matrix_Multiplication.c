a6791af30ee5edf5188c56396fa84aff0188d4d1
jaaborot <jaaborot@up.edu.ph>
2016-09-22 15:23:02 +0800

    Moved to kernel function matmul_rec_glob the the value assignment logic from intermediate function host_matmul_rec_glob.

--- Matrix_Multiplication/src/Matrix_Multiplication.c
+++ Matrix_Multiplication/src/Matrix_Multiplication.c
@@ -18,9 +18,18 @@
 // write intermediate function for kernel function matmul_rec_shar
 
 // write kernel function matmul_rec_glob
-void matmul_rec_glob(float* A_device, float* B_device, float* C_device, int blockDim_x, int blockDim_y, int blockIdx_x, int blockIdx_y, int threadIdx_x, int threadIdx_y){
-	printf(">>>>>>>> Executing kernel function matmul_rec_glob...");
-
+void matmul_rec_glob(float* A_device, float* B_device, float* C_device, int n_rows, int k_cols_rows, int m_cols, int blockDim_x, int blockDim_y, int blockIdx_x, int blockIdx_y, int threadIdx_x, int threadIdx_y){
+	printf(">>>>>>>> Executing kernel function matmul_rec_glob...\n");
+	int row = blockDim_y * blockIdx_y + threadIdx_y;
+	int col = blockDim_x * blockIdx_x + threadIdx_x;
+	if(row < n_rows && col < m_cols){
+		C_device[row * m_cols + col] = 0.0;
+		for(int cols_rows_counter = 0; cols_rows_counter < k_cols_rows; cols_rows_counter++){
+			C_device[row * m_cols + col] += A_device[row * k_cols_rows + cols_rows_counter] * B_device[cols_rows_counter * m_cols + col];
+		}
+		printf("C[%d * %d + %d = %d]: %f \n", row, m_cols, col, row * m_cols + col, C_device[row * m_cols + col]);
+//		C[row][col] = sum_{i=0}^{k-1} A[row][i]*B[i][col];
+	}
 }
 
 // write intermediate function for kernel function matmul_rec_glob
@@ -38,8 +47,8 @@ int host_matmul_rec_glob(float* A_host, float* B_host, float* C_host, int n_rows
 	printf("warp size: %d\n", warp_size);
 	printf("Number of blocks along x dim in grid: %d\n", gridDim_x);
 	printf("Number of blocks along y dim in grid: %d\n", gridDim_y);
-	printf("Number of threads along x dim in block %d\n", blockDim_x);
-	printf("Number of threads along y dim in block %d\n", blockDim_y);
+	printf("Number of threads along x dim in block: %d\n", blockDim_x);
+	printf("Number of threads along y dim in block: %d\n", blockDim_y);
 
 	// allocate device memory
 	printf(">>>> Allocating device memory...\n");
@@ -53,16 +62,20 @@ int host_matmul_rec_glob(float* A_host, float* B_host, float* C_host, int n_rows
 		for(int blockIdx_x = 0; blockIdx_x < gridDim_x; blockIdx_x++){
 			for(int threadIdx_y = 0; threadIdx_y < blockDim_y; threadIdx_y++){
 				for(int threadIdx_x = 0; threadIdx_x < blockDim_x; threadIdx_x++){
-					int row = blockDim_y * blockIdx_y + threadIdx_y;
-					int col = blockDim_x * blockIdx_x + threadIdx_x;
-					if(row < n_rows && col < m_cols){
-						C_host[row * m_cols + col] = 0.0;
-						for(int cols_rows_counter = 0; cols_rows_counter < k_cols_rows; cols_rows_counter++){
-							C_host[row * m_cols + col] += A_host[row * k_cols_rows + cols_rows_counter] * B_host[cols_rows_counter * m_cols + col];
-						}
-						printf("C[%d * %d + %d = %d]: %f \n", row, m_cols, col, row * m_cols + col, C_host[row * m_cols + col]);
+					float* A_device = A_host;
+					float* B_device = B_host;
+					float* C_device = C_host;
+					matmul_rec_glob((float*) A_device, (float*) B_device, (float*) C_device, n_rows, k_cols_rows, m_cols, blockDim_x, blockDim_y, blockIdx_x, blockIdx_y, threadIdx_x, threadIdx_y);
+//					int row = blockDim_y * blockIdx_y + threadIdx_y;
+//					int col = blockDim_x * blockIdx_x + threadIdx_x;
+//					if(row < n_rows && col < m_cols){
+//						C_host[row * m_cols + col] = 0.0;
+//						for(int cols_rows_counter = 0; cols_rows_counter < k_cols_rows; cols_rows_counter++){
+//							C_host[row * m_cols + col] += A_host[row * k_cols_rows + cols_rows_counter] * B_host[cols_rows_counter * m_cols + col];
+//						}
+//						printf("C[%d * %d + %d = %d]: %f \n", row, m_cols, col, row * m_cols + col, C_host[row * m_cols + col]);
 	//					C[row][col] = sum_{i=0}^{k-1} A[row][i]*B[i][col];
-					}
+//					}
 				}
 			}
 		}
