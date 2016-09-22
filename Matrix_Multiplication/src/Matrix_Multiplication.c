bad44ae566629b6963f7a676a9141e30f5a5f3e0
jaaborot <jaaborot@up.edu.ph>
2016-09-22 13:11:29 +0800

    Finalized skeleton of main function. Written skeleton of intermediate function host_matmul_rec_glob.
    I am having problem with the Task feature in Nsight. Cutting and pasting
    a line associated with a task results to mis-aligned task-to-line
    metadata. I deferred adding task items to Tasks list in Nsight.

--- Matrix_Multiplication/src/Matrix_Multiplication.c
+++ Matrix_Multiplication/src/Matrix_Multiplication.c
@@ -18,8 +18,39 @@
 // write intermediate function for kernel function matmul_rec_shar
 
 // write kernel function matmul_rec_glob
+void matmul_rec_glob(float* A_device, float* B_device, float* C_device, int blockDim_x, int blockDim_y, int blockIdx_x, int blockIdx_y, int threadIdx_x, int threadIdx_y){
+	printf(">>>>>>>> Executing kernel function matmul_rec_glob...");
+
+}
 
 // write intermediate function for kernel function matmul_rec_glob
+int host_matmul_rec_glob(float* A_host, float* B_host, float* C_host, int n_, int k_, int m_){
+	printf(">>>> Executing intermediate function host_matmul_rec_glob...\n");
+
+	// set execution configuration parameters
+	printf(">>>> Setting execution configuration parameters...\n");
+
+	// allocate device memory
+	printf(">>>> Allocating device memory...\n");
+
+	// copy data from host to device
+	printf(">>>> Copying data from host to device...\n");
+
+	// execute kernel function matmul_rec_glob
+	printf(">>>> Executing kernel function matmul_rec_glob...\n");
+
+	// record execution time of kernel function
+	printf(">>>> Recording execution time of kernel function matmul_rec_glob...\n");
+
+	// copy data from device to host
+	printf(">>>> Copying data from device to host\n");
+
+	// free device memory
+	printf(">>>> Freeing device memory\n");
+
+	// return kernel execution time in milliseconds
+	return 0;
+}
 
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
@@ -36,13 +67,14 @@ int main(void) {
 	printf(">> Matrix B col count: ");
 	scanf("%d", &m);
 
-	float A[n][k];
-	float B[k][m];
-	float C[n][m];
-
 	// user input for number of iteration of program execution
 	printf("\nSpecify number of program execution iterations: ");
 	scanf("%d", &execution_count);
+
+	float A[n][k];
+	float B[k][m];
+	float C[n][m];
+	float kernel_times[execution_count][2];
 
 	printf("\nn = %d, k = %d, m = %d, execution count = %d\n\n", n, k, m, execution_count);
 	for(int execution_counter = 0; execution_counter < execution_count; execution_counter++){
@@ -72,27 +104,28 @@ int main(void) {
 		}
 
 		// call intermediate function for multiplying matrices A and B using global memory
-		printf(">> Executing kernel function matmul_rec_glob...\n");
+		printf(">> Executing intermediate function for kernel function matmul_rec_glob...\n");
+		kernel_times[execution_counter][0] = host_matmul_rec_glob((float*) A, (float*) B, (float*) C, n, k, m);
 
-		// record execution time of kernel function
-		printf(">> Recording execution time of kernel function matmul_rec_glob...\n");
+		// store execution time of kernel function matmul_rec_glob
+		printf(">> Storing execution time of kernel function matmul_rec_glob\n");
 
 		// clear device memory
 		printf(">> Resetting device memory...\n");
 
 		// call intermediate function for multiplying matrices A and B using shared memory
-		printf(">> Executing kernel function matmul_rec_shar...\n");
+		printf(">> Executing intermediate function for kernel function matmul_rec_shar...\n");
 
-		// record execution time of kernel function
-		printf(">> Recording execution time of kernel function matmul_rec_shar...\n");
+		// store execution time of kernel function matmul_rec_shar
+		printf(">> Storing execution time of kernel function matmul_rec_shar\n");
 
 		// clear device memory
 		printf(">> Resetting device memory...\n");
 
-		// write to CSV file the execution time of each kernel for current iteration
-		printf(">> Writing execution time of each kernel into CSV file...\n\n");
-
 	}
+
+	// write to CSV file the execution time of each kernel for current iteration
+	printf(">> Writing execution time of each kernel into CSV file...\n\n");
 
 	return EXIT_SUCCESS;
 }
