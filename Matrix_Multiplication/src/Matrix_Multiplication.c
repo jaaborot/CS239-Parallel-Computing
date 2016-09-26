396814242192bea35effc3cdedc4b6799887115f
jaaborot <jaaborot@up.edu.ph>
2016-09-26 17:14:12 +0800

    Written serial implementation of kernel function matmul_rec_shar. The kernel function only included the simulation of copying data from a global memory to shared memory. Computation of the corresponding element of output matrix C is not included in the kernel function. Since the copying of data to shared memory is done in serial manner, we cannot yet perform the computation. Computation is postponed until all elements of of current block of A and B are loaded into dummy variables A_device_shared and B_device_shared. Computation is done in the calling function host_matmul_rec_shar.

--- Matrix_Multiplication/src/Matrix_Multiplication.c
+++ Matrix_Multiplication/src/Matrix_Multiplication.c
@@ -14,6 +14,50 @@
 #include <math.h>
 
 // write kernel function matmul_rec_shar
+void matmul_rec_shar(float* A_device, float* A_device_shared, float* B_device, float* B_device_shared, float* C_device, int A_block_start_element, int B_block_start_element, int tile_width, int n_rows, int k_cols_rows, int m_cols, int blockDim_x, int blockDim_y, int blockIdx_x, int blockIdx_y, int threadIdx_x, int threadIdx_y, int phase_counter){
+//	printf(">>>>>>>> Executing kernel function matmul_rec_shar...\n");
+
+	int A_row = blockIdx_y * tile_width + threadIdx_y;
+	int A_col = phase_counter * tile_width + threadIdx_x;
+
+	if(A_row < n_rows && A_col < k_cols_rows){
+//		printf("A if: row: %d < n_rows: %d && col: %d < k_cols_rows: %d\n", A_row, n_rows, A_col, k_cols_rows);
+		// fetch tile data from global memory to block's shared memory
+		A_device_shared[threadIdx_y * tile_width + threadIdx_x] = A_device[A_block_start_element + (threadIdx_y * k_cols_rows) + threadIdx_x];
+	} else {
+//		printf("A else: row: %d < n_rows: %d && col: %d < k_cols_rows: %d\n", A_row, n_rows, A_col, k_cols_rows);
+		A_device_shared[threadIdx_y * tile_width + threadIdx_x] = 0.0;
+	}
+
+	/* The number of columns in A is equal to the number of rows in B, k_cols_rows,
+	 * and so we flip the propagation of block in B from column priority (propagation
+	 * of block ID through all columns and then to the next row) to row priority (propagation
+	 * of block ID through all rows and then to the next column).
+	 */
+	int B_row = phase_counter * tile_width + threadIdx_y;
+	int B_col = blockIdx_x * tile_width + threadIdx_x;
+
+	if(B_row < k_cols_rows && B_col < m_cols){
+//		printf("B if: row: %d < k_cols_rows: %d && col: %d < m_rows: %d\n", B_row, k_cols_rows, B_col, m_cols);
+		B_device_shared[threadIdx_y * tile_width + threadIdx_x] = B_device[B_block_start_element + (threadIdx_y * m_cols) + threadIdx_x];
+	}else{
+//		printf("B else: row: %d < k_cols_rows: %d && col: %d < m_rows: %d\n", B_row, k_cols_rows, B_col, m_cols);
+		B_device_shared[threadIdx_y * tile_width + threadIdx_x] = 0.0;
+	}
+	// synchronize threads here using __syncthreads();
+
+	/* Each thread of the block will load the elements in the corresponding blocks in A and B to the shared memory of the thread's block.
+	 * We can then proceed with the computation after loading the elements in corresponding blocks in A and B in the shared block.
+	 */
+//	int row = blockDim_y * blockIdx_y + threadIdx_y;
+//	int col = blockDim_x * blockIdx_x + threadIdx_x;
+//	if(row < n_rows && col < m_cols){
+//		for(int counter = 0; counter < tile_width; counter++){
+//			printf("C_device[%d,%d] = C_device[%d * %d + %d] += A_device_shared[%d][%d] * B_device_shared[%d][%d] = %f + (%f * %f) = %f\n", row, col, row, m_cols, col, threadIdx_y, counter, counter, threadIdx_y, C_device[row * m_cols + col], A_device_shared[threadIdx_y][counter], B_device_shared[counter][threadIdx_x], C_device[row * m_cols + col] + A_device_shared[threadIdx_y][counter] * B_device_shared[counter][threadIdx_x]);
+//			C_device[row * m_cols + col] += A_device_shared[threadIdx_y][counter] * B_device_shared[counter][threadIdx_x];
+//		}
+//	}
+}
 
 // write intermediate function for kernel function matmul_rec_shar
 int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows, int k_cols_rows, int m_cols){
@@ -21,7 +65,7 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 
 	// set execution configuration parameters
 	printf(">>>> Setting execution configuration parameters...\n");
-	int warp_size = 2;
+	int warp_size = 32;
 	int gridDim_x = (int)ceil((float) m_cols / warp_size); /* number of blocks in the grid's x dimension */
 	int gridDim_y = (int)ceil((float) n_rows / warp_size); /* number of blocks in the grid's y dimension */
 	int blockDim_x = warp_size; /* number of threads in a block's x dimension */
@@ -42,36 +86,37 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 
 	// execute kernel function matmul_rec_glob
 	printf(">>>> Executing kernel function matmul_rec_shar...\n");
-	for(int blockIdx_y = 0; blockIdx_y < gridDim_y; blockIdx_y++){
-		for(int blockIdx_x = 0; blockIdx_x < gridDim_x; blockIdx_x++){ // for each block in the grid
-			/* variables in device global memory
-			 */
-			float* A_device = A_host;
-			float* B_device = B_host;
-			float* C_device = C_host;
-			int tile_width = blockDim_x;
-			/* variables in device shared memory (to be declared as __shared__)
-			 * tile_width = blockDim_x = blockDim_y = warp_size
-			 */
-			float A_device_shared[tile_width][tile_width];
-			float B_device_shared[tile_width][tile_width];
+	/* variables in device global memory
+	 */
+	float* A_device = A_host;
+	float* B_device = B_host;
+	float* C_device = C_host;
+	int tile_width = blockDim_x;
 
-			// load all subset data from current tile in input matrices A and B
-			printf(">>>> Loading data from A[%d,%d]-A[%d,%d]-A[%d,%d]-A[%d,%d] and B[%d,%d]-B[%d,%d]-B[%d,%d]-B[%d,%d] into shared memory...\n",
-					(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x),
-					(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x + (tile_width - 1)),
-					(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x),
-					(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x + (tile_width - 1)),
-					(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x),
-					(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x + (tile_width - 1)),
-					(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x),
-					(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x + (tile_width - 1))
-				  );
+	for(int phase_counter = 0; phase_counter < phase_max; phase_counter++){
+		for(int blockIdx_y = 0; blockIdx_y < gridDim_y; blockIdx_y++){
+			for(int blockIdx_x = 0; blockIdx_x < gridDim_x; blockIdx_x++){ // for each block in the grid
 
-			for(int phase_counter = 0; phase_counter < phase_max; phase_counter++){
+				/* variables in device shared memory (to be declared as __shared__)
+				 * tile_width = blockDim_x = blockDim_y = warp_size
+				 */
+				float A_device_shared[tile_width][tile_width];
+				float B_device_shared[tile_width][tile_width];
+
+				// load all subset data from current tile in input matrices A and B
+				printf(">>>> Loading data from A[%d,%d]-A[%d,%d]-A[%d,%d]-A[%d,%d] and B[%d,%d]-B[%d,%d]-B[%d,%d]-B[%d,%d] into shared memory...\n",
+						(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x),
+						(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x + (tile_width - 1)),
+						(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x),
+						(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x + (tile_width - 1)),
+						(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x),
+						(blockDim_y * blockIdx_y), (blockDim_x * blockIdx_x + (tile_width - 1)),
+						(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x),
+						(blockDim_y * blockIdx_y + (tile_width - 1)), (blockDim_x * blockIdx_x + (tile_width - 1))
+					  );
+
+
 				printf("C block [%d,%d], Phase #%d...\n", blockIdx_x, blockIdx_y, phase_counter);
-//				int A_block_start_element = (blockIdx_x * tile_width) + (blockIdx_y * (tile_width * k_cols_rows)); // what column on which row in A
-//				int B_block_start_element = (blockIdx_y * tile_width) + (blockIdx_x * (tile_width * m_cols)); // what row on which column in B
 				int A_block_start_element = (phase_counter * tile_width) + (blockIdx_y * (tile_width * k_cols_rows));
 				int B_block_start_element = (blockIdx_x * tile_width) + (phase_counter * (tile_width * m_cols));
 				printf("blockIdx_y: %d\n", blockIdx_y);
@@ -83,54 +128,51 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 				for(int threadIdx_y = 0; threadIdx_y < tile_width; threadIdx_y++){
 					for(int threadIdx_x = 0; threadIdx_x < tile_width; threadIdx_x++){
 
+						matmul_rec_shar((float*) A_device, (float*) A_device_shared, (float*) B_device, (float*) B_device_shared, (float*) C_device, A_block_start_element, B_block_start_element, tile_width, n_rows, k_cols_rows, m_cols, blockDim_x, blockDim_y, blockIdx_x, blockIdx_y, threadIdx_x, threadIdx_y, phase_counter);
 //						int A_row = blockIdx_y * tile_width + threadIdx_y;
-//						int A_col = blockIdx_x * tile_width + threadIdx_x;
-						int A_row = blockIdx_y * tile_width + threadIdx_y;
-						int A_col = phase_counter * tile_width + threadIdx_x;
-
-						if(A_row < n_rows && A_col < k_cols_rows){
-							printf("A if: row: %d < n_rows: %d && col: %d < k_cols_rows: %d\n", A_row, n_rows, A_col, k_cols_rows);
-							// fetch tile data from global memory to block's shared memory
-							A_device_shared[threadIdx_y][threadIdx_x] = A_device[A_block_start_element + (threadIdx_y * k_cols_rows) + threadIdx_x];
-						} else {
-							printf("A else: row: %d < n_rows: %d && col: %d < k_cols_rows: %d\n", A_row, n_rows, A_col, k_cols_rows);
-							A_device_shared[threadIdx_y][threadIdx_x] = 0.0;
-						}
-
-						/* The number of columns in A is equal to the number of rows in B, k_cols_rows,
-						 * and so we flip the propagation of block in B from column priority (propagation
-						 * of block ID through all columns and then to the next row) to row priority (propagation
-						 * of block ID through all rows and then to the next column).
-						 */
-//						int B_row = blockIdx_x * tile_width + threadIdx_y;
-//						int B_col = blockIdx_y * tile_width + threadIdx_x;
-						int B_row = phase_counter * tile_width + threadIdx_y;
-						int B_col = blockIdx_x * tile_width + threadIdx_x;
-
-						if(B_row < k_cols_rows && B_col < m_cols){
-							printf("B if: row: %d < k_cols_rows: %d && col: %d < m_rows: %d\n", B_row, k_cols_rows, B_col, m_cols);
-							B_device_shared[threadIdx_y][threadIdx_x] = B_device[B_block_start_element + (threadIdx_y * m_cols) + threadIdx_x];
-						}else{
-							printf("B else: row: %d < k_cols_rows: %d && col: %d < m_rows: %d\n", B_row, k_cols_rows, B_col, m_cols);
-							B_device_shared[threadIdx_y][threadIdx_x] = 0.0;
-						}
+//						int A_col = phase_counter * tile_width + threadIdx_x;
+//
+//						if(A_row < n_rows && A_col < k_cols_rows){
+//							printf("A if: row: %d < n_rows: %d && col: %d < k_cols_rows: %d\n", A_row, n_rows, A_col, k_cols_rows);
+//							// fetch tile data from global memory to block's shared memory
+//							A_device_shared[threadIdx_y][threadIdx_x] = A_device[A_block_start_element + (threadIdx_y * k_cols_rows) + threadIdx_x];
+//						} else {
+//							printf("A else: row: %d < n_rows: %d && col: %d < k_cols_rows: %d\n", A_row, n_rows, A_col, k_cols_rows);
+//							A_device_shared[threadIdx_y][threadIdx_x] = 0.0;
+//						}
+//
+//						/* The number of columns in A is equal to the number of rows in B, k_cols_rows,
+//						 * and so we flip the propagation of block in B from column priority (propagation
+//						 * of block ID through all columns and then to the next row) to row priority (propagation
+//						 * of block ID through all rows and then to the next column).
+//						 */
+//						int B_row = phase_counter * tile_width + threadIdx_y;
+//						int B_col = blockIdx_x * tile_width + threadIdx_x;
+//
+//						if(B_row < k_cols_rows && B_col < m_cols){
+//							printf("B if: row: %d < k_cols_rows: %d && col: %d < m_rows: %d\n", B_row, k_cols_rows, B_col, m_cols);
+//							B_device_shared[threadIdx_y][threadIdx_x] = B_device[B_block_start_element + (threadIdx_y * m_cols) + threadIdx_x];
+//						}else{
+//							printf("B else: row: %d < k_cols_rows: %d && col: %d < m_rows: %d\n", B_row, k_cols_rows, B_col, m_cols);
+//							B_device_shared[threadIdx_y][threadIdx_x] = 0.0;
+//						}
 					}
 				}
 
-				printf("A_device_shared:\n");
-				for(int threadIdx_y = 0; threadIdx_y < blockDim_y; threadIdx_y++){
-					for(int threadIdx_x = 0; threadIdx_x < blockDim_x; threadIdx_x++){
-						printf("%f ", A_device_shared[threadIdx_y][threadIdx_x]);
-					}
-					printf("\n");
-				}
-				printf("B_device_shared:\n");
-				for(int threadIdx_y = 0; threadIdx_y < blockDim_y; threadIdx_y++){
-					for(int threadIdx_x = 0; threadIdx_x < blockDim_x; threadIdx_x++){
-						printf("%f ", B_device_shared[threadIdx_y][threadIdx_x]);
-					}
-					printf("\n");
-				}
+//				printf("A_device_shared:\n");
+//				for(int threadIdx_y = 0; threadIdx_y < blockDim_y; threadIdx_y++){
+//					for(int threadIdx_x = 0; threadIdx_x < blockDim_x; threadIdx_x++){
+//						printf("%f ", (float) A_device_shared[threadIdx_y][threadIdx_x]);
+//					}
+//					printf("\n");
+//				}
+//				printf("B_device_shared:\n");
+//				for(int threadIdx_y = 0; threadIdx_y < blockDim_y; threadIdx_y++){
+//					for(int threadIdx_x = 0; threadIdx_x < blockDim_x; threadIdx_x++){
+//						printf("%f ", (float) B_device_shared[threadIdx_y][threadIdx_x]);
+//					}
+//					printf("\n");
+//				}
 
 				// compute using loaded subset data on current tile of input matrices A and B
 				printf(">>>> Computing subset of output matrix using data in block\n");
@@ -141,7 +183,7 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 						int col = blockDim_x * blockIdx_x + threadIdx_x;
 						if(row < n_rows && col < m_cols){
 							for(int counter = 0; counter < tile_width; counter++){
-								printf("C_device[%d,%d] = C_device[%d * %d + %d] += A_device_shared[%d][%d] * B_device_shared[%d][%d] = %f + (%f * %f) = %f\n", row, col, row, m_cols, col, threadIdx_y, counter, counter, threadIdx_y, C_device[row * m_cols + col], A_device_shared[threadIdx_y][counter], B_device_shared[counter][threadIdx_x], C_device[row * m_cols + col] + A_device_shared[threadIdx_y][counter] * B_device_shared[counter][threadIdx_x]);
+//								printf("C_device[%d,%d] = C_device[%d * %d + %d] += A_device_shared[%d][%d] * B_device_shared[%d][%d] = %f + (%f * %f) = %f\n", row, col, row, m_cols, col, threadIdx_y, counter, counter, threadIdx_y, C_device[row * m_cols + col], A_device_shared[threadIdx_y][counter], B_device_shared[counter][threadIdx_x], C_device[row * m_cols + col] + A_device_shared[threadIdx_y][counter] * B_device_shared[counter][threadIdx_x]);
 								C_device[row * m_cols + col] += A_device_shared[threadIdx_y][counter] * B_device_shared[counter][threadIdx_x];
 							}
 						}
@@ -154,7 +196,7 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 						int row = blockDim_y * blockIdx_y + threadIdx_y;
 						int col = blockDim_x * blockIdx_x + threadIdx_x;
 						if(row < n_rows && col < m_cols){
-							printf("%f ", C_device[row * tile_width + col]);
+							printf("%f ", C_device[row * m_cols + col]);
 						}
 					}
 					printf("\n");
@@ -162,7 +204,6 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 			}
 		}
 	}
-
 	// record execution time of kernel function
 	printf(">>>> Recording execution time of kernel function matmul_rec_shar...\n");
 
@@ -187,7 +228,6 @@ void matmul_rec_glob(float* A_device, float* B_device, float* C_device, int n_ro
 			C_device[row * m_cols + col] += A_device[row * k_cols_rows + cols_rows_counter] * B_device[cols_rows_counter * m_cols + col];
 		}
 		printf("C[%d * %d + %d = %d]: %f \n", row, m_cols, col, row * m_cols + col, C_device[row * m_cols + col]);
-//		C[row][col] = sum_{i=0}^{k-1} A[row][i]*B[i][col];
 	}
 }
 
