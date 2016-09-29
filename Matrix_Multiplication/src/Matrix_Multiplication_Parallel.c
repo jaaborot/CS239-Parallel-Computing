69eb598f9cc35396de469eee5ecd30bd583a39b3
jaaborot <jaaborot@up.edu.ph>
2016-09-29 16:42:31 +0800

    Added new C class Matrix_Multiplication_Parallel.c for CUDA implementation of serial functions in class Matrix_Multiplication.c. Finished writing kernel function matmul_rec_glob. Next step is to write kernel function matmul_rec_shar.

--- Matrix_Multiplication/src/Matrix_Multiplication_Parallel.c
+++ Matrix_Multiplication/src/Matrix_Multiplication_Parallel.c
@@ -218,81 +218,168 @@ int host_matmul_rec_shar(float* A_host, float* B_host, float* C_host, int n_rows
 }
 
 // write kernel function matmul_rec_glob
-void matmul_rec_glob(float* A_device, float* B_device, float* C_device, int n_rows, int k_cols_rows, int m_cols, int blockDim_x, int blockDim_y, int blockIdx_x, int blockIdx_y, int threadIdx_x, int threadIdx_y){
+__global__ void matmul_rec_glob(float* A_device, float* B_device, float* C_device, int n_rows, int k_cols_rows, int m_cols){
 //	printf(">>>>>>>> Executing kernel function matmul_rec_glob...\n");
-	int row = blockDim_y * blockIdx_y + threadIdx_y;
-	int col = blockDim_x * blockIdx_x + threadIdx_x;
+	int row = blockDim.y * blockIdx.y + threadIdx.y;
+	int col = blockDim.x * blockIdx.x + threadIdx.x;
 	if(row < n_rows && col < m_cols){
 		C_device[row * m_cols + col] = 0.0;
 		for(int cols_rows_counter = 0; cols_rows_counter < k_cols_rows; cols_rows_counter++){
+//			printf("C[%d] = C[%d * %d + %d] + A[ %d * %d + %d] * B[%d * %d + %d] = C[%d] + A[%d] * B[%d] = %f + %f * %f\n",
+//					(row * m_cols + col),
+//					row,
+//					m_cols,
+//					col,
+//					row,
+//					k_cols_rows,
+//					cols_rows_counter,
+//					cols_rows_counter,
+//					m_cols,
+//					col,
+//					(row * m_cols + col),
+//					(row * k_cols_rows + cols_rows_counter),
+//					(cols_rows_counter * m_cols + col),
+//					C_device[row * m_cols + col],
+//					A_device[row * k_cols_rows + cols_rows_counter],
+//					B_device[cols_rows_counter * m_cols + col]);
 			C_device[row * m_cols + col] += A_device[row * k_cols_rows + cols_rows_counter] * B_device[cols_rows_counter * m_cols + col];
+			// C[4 * 3 + 0] = 0 + A[4 * 4 + 0] * B[0 * 3 + 0] = A[16]*B[0]
+			// C[4 * 3 + 0] = (A[16] * B[0]) + A[4 * 4 + 1] * B[1 * 3 + 0] = A[16]*B[0] + A[17]*B[3]
+			// C[4 * 3 + 0] = (A[16]*B[0] + A[17]*B[3]) + A[4 * 4 + 2] * B[2 * 3 + 0] = A[16]*B[0] + A[17]*B[3] + A[18]*B[6]
+			// C[4 * 3 + 0] = (A[16]*B[0] + A[17]*B[3] + A[18]*B[6]) + A[4 * 4 + 3] * B[3 * 3 + 0] = A[16]*B[0] + A[17]*B[3] + A[18]*B[6] + A[19]*B[9]
+
 		}
-//		printf("C[%d * %d + %d = %d]: %f \n", row, m_cols, col, row * m_cols + col, C_device[row * m_cols + col]);
+		__syncthreads();
+//		printf("C[%d * %d + %d] = C[%d]: %f \n", row, m_cols, col, row * m_cols + col, C_device[row * m_cols + col]);
 	}
 }
 
 /*
- * Serial implementation of kernel matmul_rec_glob.
+ * Intermediate function for kernel function matmul_rec_glob.
  */
 int host_matmul_rec_glob(float* A_host, float* B_host, float* C_host, int n_rows, int k_cols_rows, int m_cols){
 	printf(">>>> Executing intermediate function host_matmul_rec_glob...\n");
+//	printf("A_host:\n");
+//	for(int row = 0; row < n_rows; row++){
+//		for(int col = 0; col < k_cols_rows; col++){
+//			printf("%f", A_host[row * k_cols_rows + col]);
+//		}
+//		printf("\n");
+//	}
+//	printf("\n");
+//	printf("A_host:\n");
+//	for(int row = 0; row < n_rows; row++){
+//		for(int col = 0; col < k_cols_rows; col++){
+//			printf("%f", A_host[row * k_cols_rows + col]);
+//		}
+//		printf("\n");
+//	}
+//	printf("\n");
 
 	// set execution configuration parameters
-	printf(">>>> Setting execution configuration parameters...\n");
-	int warp_size = 32;
-	int gridDim_x = (int)ceil((float) m_cols / warp_size); /* number of blocks in the grid's x dimension */
-	int gridDim_y = (int)ceil((float) n_rows / warp_size); /* number of blocks in the grid's y dimension */
-	int blockDim_x = warp_size; /* number of threads in a block's x dimension */
-	int blockDim_y = warp_size; /* number of threads in a block's y dimension */
+	// Reference: CUDA by Example, Sanders et al., 2011
 
-	printf("warp size: %d\n", warp_size);
-	printf("Number of blocks along x dim in grid: %d\n", gridDim_x);
-	printf("Number of blocks along y dim in grid: %d\n", gridDim_y);
-	printf("Number of threads along x dim in block: %d\n", blockDim_x);
-	printf("Number of threads along y dim in block: %d\n", blockDim_y);
+	printf(">>>> Setting execution configuration parameters...\n");
+	cudaDeviceProp prop;
+	int cuda_device_count; // number of CUDA-enabled devices
+	int gridDim_x; /* number of blocks in the grid's x dimension */
+	int gridDim_y; /* number of blocks in the grid's y dimension */
+	int blockDim_x; /* number of threads in a block's x dimension */
+	int blockDim_y; /* number of threads in a block's y dimension */
+
+	cudaGetDeviceCount(&cuda_device_count);
+	if(cuda_device_count > 0){
+		cudaGetDeviceProperties(&prop, 0);
+		printf(">> Device properties ==\n");
+		printf("CUDA-enabled device count: %d\n", cuda_device_count);
+		printf("Maximum number of threads per 2D block: %d\n", prop.maxThreadsPerBlock); // maximum number of threads which can be alloted to a single block
+		printf("Maximum number of threads in x, y and z dimension of a 3D block: %d, %d, %d\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]); // maximum number of threads which can be alloted along the x, y and z dimension of each block
+		printf("Maximum number of threads in x, y and z dimension of a 3D grid: %d, %d, %d\n\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+		/* The programmer may design each block with varying number of threads along the x and y dimension as long as
+		 * the total number of threads in each block does not exceed maxThreadsPerBlock. For example, if maxThreadsPerBlock = 1024,
+		 * the programmer may design the block to be a 32 rows x 32 columns block, 1 row by 1024 columns block, 1024 row by 1 column block, etc.
+		 */
+
+		/* maxGridSize refers to the maximum number of threads a grid can hold along the x (maxGridSize[0]), y (maxGridSize[1]) and z (maxGridSize[2]) dimensions.
+		 * If the programmer wants to compute for the output matrix in a single step, then the maximum dimensions of the input matrices will be limited by the
+		 * maximum number of threads a grid can have, i.e. maxGridSize[0] > dimension and maxGridSize[1] > dimension.
+		 */
+		if(prop.maxGridSize[0] > m_cols && prop.maxGridSize[1] > n_rows){ // if matrix size less than number of threads in a grid
+
+			// determine block dimension of the grid
+			gridDim_x = (int)ceil((float) m_cols / prop.warpSize); // necessary number of blocks in x dimension in the grid
+			gridDim_y = (int)ceil((float) n_rows / prop.warpSize); // necessary number of blocks in y dimension in the grid
+
+			// determine thread dimension of each block
+			blockDim_x = prop.warpSize;
+			blockDim_y = prop.warpSize;
+
+			printf("warp size: %d\n", prop.warpSize);
+			printf("Number of blocks along x dim in grid: %d\n", gridDim_x);
+			printf("Number of blocks along y dim in grid: %d\n", gridDim_y);
+			printf("Number of threads along x dim in block: %d\n", blockDim_x);
+			printf("Number of threads along y dim in block: %d\n", blockDim_y);
+
+		}else{
+
+			// exit with max-thread-limit-reached exception
+			printf(">> Number of matrix elements exceeded maximum number of threads available.\n");
+			return 1;
+
+		}
+	}else{
+		printf(">> No CUDA-enabled device found.\n");
+		return 1;
+	}
+
+	dim3 blocks_per_grid(gridDim_x, gridDim_y, 1); // how many blocks to utilize in the grid
+	dim3 threads_per_block(blockDim_x, blockDim_y, 1); // how many threads to utilize per block in the grid
+
+	// configure execution parameters
+	int size = n_rows * m_cols * sizeof(float);
+	float* A_device;
+	float* B_device;
+	float* C_device;
 
 	// allocate device memory
 	printf(">>>> Allocating device memory...\n");
+	cudaMalloc((void**) &A_device, n_rows * k_cols_rows * sizeof(float));
+	cudaMalloc((void**) &B_device, k_cols_rows * m_cols * sizeof(float));
+	cudaMalloc((void**) &C_device, n_rows * m_cols * sizeof(float));
 
 	// copy data from host to device
 	printf(">>>> Copying data from host to device...\n");
+	cudaMemcpy(A_device, A_host, n_rows * k_cols_rows * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(B_device, B_host, k_cols_rows * m_cols * sizeof(float), cudaMemcpyHostToDevice);
 
 	// execute kernel function matmul_rec_glob
 	printf(">>>> Executing kernel function matmul_rec_glob...\n");
-	for(int blockIdx_y = 0; blockIdx_y < gridDim_y; blockIdx_y++){
-		for(int blockIdx_x = 0; blockIdx_x < gridDim_x; blockIdx_x++){
-			for(int threadIdx_y = 0; threadIdx_y < blockDim_y; threadIdx_y++){
-				for(int threadIdx_x = 0; threadIdx_x < blockDim_x; threadIdx_x++){
-					float* A_device = A_host;
-					float* B_device = B_host;
-					float* C_device = C_host;
-					matmul_rec_glob((float*) A_device, (float*) B_device, (float*) C_device, n_rows, k_cols_rows, m_cols, blockDim_x, blockDim_y, blockIdx_x, blockIdx_y, threadIdx_x, threadIdx_y);
-//					int row = blockDim_y * blockIdx_y + threadIdx_y;
-//					int col = blockDim_x * blockIdx_x + threadIdx_x;
-//					if(row < n_rows && col < m_cols){
-//						C_host[row * m_cols + col] = 0.0;
-//						for(int cols_rows_counter = 0; cols_rows_counter < k_cols_rows; cols_rows_counter++){
-//							C_host[row * m_cols + col] += A_host[row * k_cols_rows + cols_rows_counter] * B_host[cols_rows_counter * m_cols + col];
-//						}
-//						printf("C[%d * %d + %d = %d]: %f \n", row, m_cols, col, row * m_cols + col, C_host[row * m_cols + col]);
-	//					C[row][col] = sum_{i=0}^{k-1} A[row][i]*B[i][col];
-//					}
-				}
-			}
-		}
-	}
-
-	// record execution time of kernel function
-	printf(">>>> Recording execution time of kernel function matmul_rec_glob...\n");
+	// define CUDA event recorder
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+	// execute kernel function
+	matmul_rec_glob<<<blocks_per_grid, threads_per_block>>>(A_device, B_device, C_device, n_rows, k_cols_rows, m_cols);
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	// compute for elapsed time (execution time of kernel)
+	float runtime = 0;
+	cudaEventElapsedTime(&runtime, start, stop);
+	cudaDeviceSynchronize();
 
 	// copy data from device to host
 	printf(">>>> Copying data from device to host\n");
+	cudaMemcpy(C_host, C_device, n_rows * m_cols * sizeof(float), cudaMemcpyDeviceToHost);
 
 	// free device memory
 	printf(">>>> Freeing device memory\n");
+	cudaFree(A_device);
+	cudaFree(B_device);
+	cudaFree(C_device);
 
 	// return kernel execution time in milliseconds
-	return 0;
+	return (int)ceil(runtime);
 }
 
 int main(void) {
@@ -365,9 +452,6 @@ int main(void) {
 			}
 			printf("\n");
 		}
-
-		// store execution time of kernel function matmul_rec_glob
-		printf(">> Storing execution time of kernel function matmul_rec_glob\n");
 
 		// clear device memory
 		printf(">> Resetting device memory...\n");
